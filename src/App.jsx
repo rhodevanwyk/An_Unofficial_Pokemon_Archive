cf3df76bf973b536a05e570e9510d813d8bd82d0
rhode <vanwykrhode30@gmail.com>
2026-07-13 18:18:13 +0200

    front end design, fetch info from url

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,70 @@
+import { use, useState } from "react";
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [pokemon, setPokemon] = useState("");
+  const [loading, setLoading] = useState("");
+  const [error, setError] = useState("");
+
+  const fetchPokemon = async (name) => {
+    if (!name.trim()) return;
+
+    setPokemon(null);
+    setLoading(true);
+    setError(null);
+
+    try {
+      const res = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`,
+      );
+      if (!res.ok) {
+        throw new Error("Your Pokémon Was Not Found");
+      }
+      const data = await res.json();
+      setPokemon(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-red-600">Pokédex Mini</h1>
+    <div className="min-h-screen bg-[url('./assets/bg.jpg')] bg-cover bg-center bg-no-repeat text-[#FFCB05] p-16 flex flex-col justify-center items-center">
+      <img
+        src="src/assets/pokemon_logo.png"
+        alt="Main Pokémon Logo"
+        className="w-[30%] mb-10"
+      />
+      <div className="bg-white/10 backdrop-blur-md border border-white/20 p-10 rounded-xl text-white shadow-xl w-[30%] text-center">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search Your Pokémon"
+          className="flex-1 focus:outline-none p-4 rounded-xl text-[#4A4A4A] bg-gray-50 font-bold w-[80%]"
+        />
+        <button
+          onClick={() => fetchPokemon(searchTerm)}
+          className="ml-2 p-4 text-gray-50 bg-[#4A4A4A] rounded-xl font-bold"
+        >
+          <i className="fa-solid fa-magnifying-glass font-bold"></i>
+        </button>
+
+        {loading && <p className="mt-8 text-[#4A4A4A] font-bold">Loading...</p>}
+        {error && (
+          <p className="text-[#C23B22] mt-8 font-bold text-center">{error}</p>
+        )}
+        {pokemon && (
+          <div className="max-h-80 overflow-y-auto mt-8 p-8 bg-[#121212]/50 rounded-lg text-left">
+            <pre className="text-xs text-wrap break-all">
+              {JSON.stringify(pokemon, null, 2)}
+            </pre>
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
